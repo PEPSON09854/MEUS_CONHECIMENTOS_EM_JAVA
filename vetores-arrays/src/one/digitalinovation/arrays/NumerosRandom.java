a797e5dc07bc3d260844eddcba237e6209683f57
Pepson Anderson <pepsonanderson@hotmail.com>
2023-03-30 11:56:12 -0300

    trabalhando com arrays

---
+++ vetores-arrays/src/one/digitalinovation/arrays/NumerosRandom.java
@@ -0,0 +1,41 @@
+package one.digitalinovation.arrays;
+
+import java.util.Random;
+
+public class NumerosRandom {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int[] numberRandom = new int[20];
+
+        for (int i = 0; i < numberRandom.length; i++) {
+
+            int number = random.nextInt(100);
+            numberRandom[i] = number;
+
+        }
+
+        System.out.print("\nNumeros aleatorios: ");
+        for (int num : numberRandom) {
+            System.out.print(num + " ");
+
+        }
+
+        System.out.print("\nNumeros sucessores aleatorios: ");
+        for (int num : numberRandom) {
+            System.out.print((num + 1) + " ");
+
+        }
+
+        System.out.print("\nNumeros antecessores aleatorios: ");
+
+        for (int num : numberRandom) {
+            System.out.print((num - 1) + " ");
+
+        }
+
+    }
+
+}
---
+++ vetores-arrays/src/one/digitalinovation/arrays/PraticandoArray.java
@@ -0,0 +1,23 @@
+package one.digitalinovation.arrays;
+
+public class PraticandoArray {
+    public static void main(String[] args) {
+
+        int[] vetor = { 10, 20, 30, 40, 50 };
+
+        System.out.print("Vetor ");
+        int count = 0;
+        while (count <= (vetor.length - 1)) {
+
+            System.out.print(vetor[count] + " ");
+            count++;
+        }
+
+        System.out.print("\nInverso ");
+
+        for (int i = (vetor.length - 1); i >= 0; i--) {
+            System.out.print(vetor[i] + " ");
+        }
+    }
+
+}
